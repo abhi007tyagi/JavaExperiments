d4f91a8b210c34606ad1c43d011f2a594c1e8e84
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2016-12-06 23:54:35 +0530

    Updated for Hacker Rank Linked List

--- JavaExperiments/src/tyagiabhinav/util/Node.java
+++ JavaExperiments/src/tyagiabhinav/util/Node.java
@@ -9,10 +9,13 @@ package tyagiabhinav.util;
  */
 public class Node {
 
-	public int val;
-	public Node next,prev,parent,left,right;
-	
-	public Node(int n){
+	public int val, data;
+	public Node next, prev, parent, left, right;
+
+	public Node() {
+	}
+
+	public Node(int n) {
 		this.val = n;
 		this.next = this.prev = this.parent = this.left = this.right = null;
 	}
