49294376248f99d4b79fd29c384e3953a8922a36
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-07-04 22:24:37 +0530

    CaesarCipher done

--- JavaExperiments/src/tyagiabhinav/hackerrank/CaesarCipher.java
+++ JavaExperiments/src/tyagiabhinav/hackerrank/CaesarCipher.java
@@ -19,22 +19,31 @@ public class CaesarCipher {
 		int n = in.nextInt();
 		String s = in.next();
 		int k = in.nextInt();
+		k = k % 26; // to remove multiples of 26 and get the exact value we need
+					// to shift
 
 		StringBuilder strBuilder = new StringBuilder(n);
 		for (int i = 0; i < n; i++) {
 			char ch = s.charAt(i);
+			int chInt = (int) ch;
+
 			if (Character.isLetter(ch)) {
-				if (!Character.isLetter((char) (ch + k))) {
+				if (chInt >= 65 && chInt <= 90 && (chInt + k) >= 97) {
 					strBuilder.append((char) (ch + k - 26));
 				} else {
-					strBuilder.append((char) (ch + k));
+					chInt += k;
+
+					if (chInt >= 65 && chInt <= 90 || chInt >= 97 && chInt <= 122) {
+						strBuilder.append((char) (ch + k));
+					} else if ((chInt > 90 && chInt < 97) || chInt > 122) {
+						strBuilder.append((char) (ch + k - 26));
+					}
 				}
 			} else {
 				strBuilder.append(ch);
 			}
 		}
 		System.out.println(strBuilder.toString());
-
 	}
 
 }
