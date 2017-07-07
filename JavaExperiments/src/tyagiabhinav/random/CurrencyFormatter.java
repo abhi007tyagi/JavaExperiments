6c947ab36cbe3e4204c5d502729cb4395c2d0db5
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-07-07 21:10:23 +0530

    Currency Formatter

---
+++ JavaExperiments/src/tyagiabhinav/random/CurrencyFormatter.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package tyagiabhinav.random;
+
+import java.text.DecimalFormat;
+
+/**
+ * @author abhinavtyagi
+ *
+ */
+public class CurrencyFormatter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DecimalFormat decimalFormat = new DecimalFormat("###,###,###.##");
+		String formatted = decimalFormat.format(1234.678);
+		System.out.println(formatted);
+	}
+
+}
